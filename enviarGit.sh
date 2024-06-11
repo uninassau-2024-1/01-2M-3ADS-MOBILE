99f9ba8158a6d7988d9ca90e409d7dce5ac2ac68
João Ferreira <joaoferreirape@gmail.com>
2024-06-10 22:05:16 -0300

    Repositório fechado.

--- enviarGit.sh
+++ enviarGit.sh
@@ -3,7 +3,12 @@ find . -type d -name '.nx'          -exec rm -rf {} +
 find . -type d -name '.vscode'      -exec rm -rf {} +
 find . -type d -name 'node_modules' -exec rm -rf {} +
 
-find GRP* -print | sort | while read filename; do touch $filename; done
+find GRP* -print | sort | while read filename; do
+    # touch $filename;
+    git rm -r --cached $filename;
+    git add $filename;
+    git update-index --no-assume-unchanged $filename
+done
 
 git add .
 git commit -m 'Repositório fechado.'
